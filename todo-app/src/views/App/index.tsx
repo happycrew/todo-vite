5a2571f7c36804227e225118b3d33ee68fce85fa
Vladimir Orlov <satancrew.true@gmail.com>
2023-06-13 17:08:44 +0300

    feat: add store by zustand

--- todo-app/src/views/App/index.tsx
+++ todo-app/src/views/App/index.tsx
@@ -1,7 +1,17 @@
 import { FC } from 'react';
+import { useTodoStore } from '@/data/stores/useToDoStore';
 import styles from './index.module.scss';
 
 export const App: FC = () => {
+  console.log(useTodoStore);
+
+  const [tasks, createTask, updateTask, removeTask] = useTodoStore(state => [
+    state.tasks,
+    state.createTask,
+    state.updateTask,
+    state.removeTask,
+  ]); 
+
   return (
     <article className={styles.app}>
       <h1 className={styles.appTitle}>To Do App</h1>
