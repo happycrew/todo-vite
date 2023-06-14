8958cbac476e061565727641dcf6d50e7d412260
Vladimir Orlov <86786994+Satancrew@users.noreply.github.com>
2023-06-14 14:34:05 +0300

    feat: add tasks to list and edit/deleting tasks (#4)

--- todo-app/src/data/stores/useToDoStore.ts
+++ todo-app/src/data/stores/useToDoStore.ts
@@ -4,6 +4,7 @@ import { generateId } from '../helpers';
 type Task = {
   id: string;
   title: string;
+  done: boolean;
 };
 
 type TodoStore = {
@@ -11,21 +12,32 @@ type TodoStore = {
   createTask: (title: string) => void;
   updateTask: (id: string, title: string) => void;
   removeTask: (id: string) => void;
+  doneTask: (id: string) => void;
 };
 
 export const useTodoStore = create<TodoStore>((set, get) => ({
-  tasks: [],
+  tasks: [
+    {
+      id: 'gfgfgfgfgfgfgfgf',
+      title: 'Drink Coffe',
+      done: false,
+    },
+    { id: 'gfgjfjdfgfgffgfgg', title: 'Sleeping', done: false },
+  ],
+  
   createTask: (title: string) => {
     const { tasks } = get();
     const newTask = {
       id: generateId(),
       title: title,
+      done: false,
     };
 
     set({
       tasks: [newTask].concat(tasks),
     });
   },
+
   updateTask: (id: string, title: string) => {
     const { tasks } = get();
     set({
@@ -35,10 +47,28 @@ export const useTodoStore = create<TodoStore>((set, get) => ({
       })),
     });
   },
+
   removeTask: (id: string) => {
     const { tasks } = get();
     set({
       tasks: tasks.filter((task: Task) => task.id !== id),
+    });
+  },
+
+  doneTask: (id: string) => {
+    const { tasks } = get();
+    const updatedTasks = tasks.map((task: Task) => {
+      if (task.id === id) {
+        return {
+          ...task,
+          done: !task.done,
+        };
+      }
+      return task;
+    });
+
+    set({
+      tasks: updatedTasks,
     });
   },
 }));
